82a082de2b49799b27f0d2e1bc4024a1820694b7
genna-cervantes <gennacervantes9@gmail.com>
2024-10-11 13:01:24 +0800

    feat: keep track of lines

--- Main.java
+++ Main.java
@@ -14,69 +14,65 @@ class Line {
     }
 }
 
+class Token {
+    
+    public int line;
+    public String token;
+    public int charCount;
+
+    public Token(int line, String token) {
+        this.line = line;
+        this.token = token;
+    }
+}
+
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("Please type the if else then statement to the console (type \\q to exit):");
         Scanner sc = new Scanner(System.in);
-        // ArrayList<Lexeme> inputLexemes = new ArrayList<>();
-        ArrayList<String> inputLines = new ArrayList<>();
+        
+        ArrayList<Line> inputLines = new ArrayList<>();
         String inputLine = "";
         int lineCounter = 0;
 
         while (!inputLine.equals("\\q")) {
             inputLine = sc.nextLine();
 
-            if (inputLine == "") {
+            if (inputLine.equals("")) {
                 lineCounter++;
                 continue;
             }
 
-            // String[] splitStrings = inputLine.split("\\s+");
-            // for (String s: splitStrings){
-            //     Lexeme lex = new Lexeme(lineCounter, s);
-            //     inputLexemes.add(lex);
-            // }
-            inputLines.add(inputLine);
+            Line line = new Line(lineCounter, inputLine);
+
+            inputLines.add(line);
             lineCounter++;
         }
 
         sc.close();
 
         LexicalAnalyzer la = new LexicalAnalyzer();
-        ArrayList<String> tokens = new ArrayList<>();
+        ArrayList<Token> tokens = new ArrayList<>();
 
-        // for (int i = 0; i < inputLexemes.size(); i++){
-        //     if (i >= inputLexemes.size() - 1){
-        //         break;
-        //     }
-        //     Lexeme l = inputLexemes.get(i);
-        //     String token = la.getToken(l.lex, l.line);
-        //     // syntax analysis part
-        //     tokens.add(token);
-        // }
-        // System.out.println("hello");
-        // SyntaxAnalyzer sa = new SyntaxAnalyzer(tokens);
-        // sa.parseIfThenElse();
+        for (Line l: inputLines){
 
-        for (int i = 0; i < inputLines.size(); i++){
-
-            String line = inputLines.get(i);
-
-            if (i >= inputLines.size() - 1) {
+            // ignore last line "\q"
+            if (l.line >= lineCounter - 1){
                 break;
             }
 
-            ArrayList<String> tokensInLine = la.tokenizeString(line);
+            ArrayList<String> tokensInLine = la.tokenizeString(l.str);
 
             for (String t : tokensInLine) {
-                tokens.add(t);
+                Token token = new Token(l.line, t);
+                tokens.add(token);
             }
         }
 
-        for (String s : tokens) {
-            System.out.println(s);
+        for (Token t : tokens) {
+            System.out.println(t.token + t.line);
         }
 
     }
